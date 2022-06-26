41299bb2b565c9c0e287aea2d24c5a171f6ea3de
saumss <73573544+saumss@users.noreply.github.com>
2022-06-26 18:03:42 +0530

    Add files via upload

---
+++ MLProject/_💻_Predict.py
@@ -0,0 +1,54 @@
+import pickle
+import streamlit as st
+import time
+
+# loading in the model to predict on the data
+pickle_model = open('modelLogi.pkl', 'rb')
+model = pickle.load(pickle_model)
+
+def welcome():
+    return 'welcome all'
+
+# defining the function which will make the prediction using
+# the data which the user inputs
+def prediction(elo_n_pred, elo_i_pred, forecast):
+    prediction = model.predict(
+        [[elo_n_pred, elo_i_pred, forecast]])
+    print(prediction)
+    return prediction
+
+def main():
+
+    html_temp = """
+	<div style ="background-color:lightblue;padding:13px">
+	  <h1 style ="color:black;text-align:center;">Streamlit NBA Game Outcome App </h1>
+	</div>
+	<br>
+	"""
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    hide_menu_style = """
+    <style>
+    
+    footer {visibility: hidden; }
+    </style>
+    """
+    st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+    elo_i_pred = st.slider("Elo Prediction (Before):", 0.00, 1.00)
+    elo_n_pred = st.slider("Elo Prediction (After):", 0.00, 1.00)
+    forecast = st.slider("Forecast:", 0.00, 1.00)
+    gameresult = ""
+
+    if st.button("Predict"):
+        gameresult = prediction(elo_n_pred, elo_i_pred, forecast)
+        with st.spinner("Loading..."):
+            time.sleep(5)
+    if gameresult == 1:
+        st.success('The Team will: win')
+        st.balloons()
+    if gameresult == 0:
+           st.warning('The Team will: lose')
+
+if __name__ == '__main__':
+    main()
---
+++ MLProject/pages/_🏀_FAQs.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import os
+from PIL import Image
+dir_root = os.path.dirname(os.path.abspath(__file__))
+logo = Image.open(dir_root+'/nba_image.png')
+st.image(logo)
+with st.expander("History of NBA"):
+    st.write("""
+        The National Basketball Association, or NBA, is a professional basketball league comprised of 30 teams across 
+        North America featuring the best basketball players in the world.
+    """)
+with st.expander("What is ELo Ranking?"):
+    st.write("""
+            Elo ratings keep track of the final score of each game, 
+where the game was played, and when the game was played. A 1500 Elo rating is the average
+and starting rating. This score changes as the season progresses adding or subtracting to 
+account for the team’s performance.
+        """)
+with st.expander("Currently, who is the leading NBA team?"):
+    st.write("""
+    As per 'Points Per Game' stats, Memphis Grizzlies leads the leaderboard with a 112.5 points""")
